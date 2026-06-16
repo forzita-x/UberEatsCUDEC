26e0c288ce3b160d083642843e3002894486c21c
MarcosRios7 <marcosrios246@gmail.com>
2026-06-15 21:19:50 -0600

    El dia de hoy configuramos en la aplicacion, mas especifico en db.js e index.js, el agregar y borrar platillos al igual que la actualizacion en el apartado, en el que usamos el onSnapshot, la parte que actualiza la app si se borrar

---
+++ js/db.js
@@ -0,0 +1,44 @@
+db.collection("platillos").onSnapshot((coleccion) => {
+    coleccion.docChanges().forEach((registro) => {
+        if (registro.type === "added"){
+            mostrarPlatillo(registro.doc.data(), registro.doc.id);
+        }
+        if (registro.type === "modified"){
+            actualizarPlatillo(registro.doc.data(), registro.doc.id);
+        }
+        if (registro === "removed") {
+            borrarPlatillo(registro.doc.id);
+        }
+    });
+});
+
+const formularioAgregar = document.querySelector("form");
+formularioAgregar.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const platilloNuevo = {
+        nombre: formularioAgregar.nombre.value,
+        ingredientes: formularioAgregar.ingredientes.value,
+        precio: formularioAgregar.precio.value
+    }
+    db.collection("platillos").add(platilloNuevo)
+    .catch((error) => {
+        console.log(error);
+        alert("Error al agregar platillo");
+    }
+    );
+
+    formularioAgregar.nombre.value = "";
+    formularioAgregar.ingredientes.value = "";
+    formularioAgregar.precio.value = "";
+    alert("Platillo agregado");
+});
+
+const platilloBorrar = document.querySelector(".recipes");
+platilloBorrar.addEventListener("click",(e) => {
+    if (e.target.tagName === 'I'){
+        const id = e.target.getAttribute("data-id");
+        db.collection("platillos").doc(id).delete();
+
+    }
+    confirm("¿Estas seguro de borrar el platillo");
+})
--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+let contenido = "";
+
 document.addEventListener('DOMContentLoaded', function() {
   // nav menu
   const menus = document.querySelectorAll('.side-menu');
@@ -6,3 +8,36 @@ document.addEventListener('DOMContentLoaded', function() {
   const forms = document.querySelectorAll('.side-form');
   M.Sidenav.init(forms, {edge: 'left'});
 });
+
+function mostrarPlatillo(platillo, id) {
+  contenido = `
+    <div class='card-panel recipe white row' id='${id}' data-id='${id}'>
+        <div class='recipe-details'>
+
+          <div class='recipe-title'>${platillo.nombre}</div>
+          <div class='recipe-ingredients'>${platillo.ingredientes}</div>
+          <div class='recipe-precio'>Precio: $${platillo.precio} MXN</div>
+
+          <div class="recipe-delete">
+            <i class="material-icons" data-id='${id}'>
+            delete_outline
+            </i>
+          </div>
+
+        </div>
+    </div>
+  `;
+    document.querySelector(".recipes").innerHTML += contenido;
+};
+
+function actualizarPlatillo(platillo,id){
+  let tarjeta = document.getElementById(`${id}`);
+  tarjeta.querySelector(".recipe-title").innerHTML = platillo.nombre;
+  tarjeta.querySelector(".recipe-ingredientes").innerHTML = platillo.ingredientes;
+  tarjeta.querySelector(".recipe-precio").innerHTML = platillo.precio;
+}
+
+const borrarPlatillo = (id) => {
+const platillo = document.querySelector(`.recipe[data-id=${id}]`);
+platillo.remove();
+};
