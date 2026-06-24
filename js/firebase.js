90b5c29feb69b289e23049f04c6c9e23ea1c2dd1
RipperRoo777 <davidgoveaconversa@gmail.com>
2026-06-23 20:53:21 -0600

    YOLLL

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAeaYdjjiIArA3dFOoyTjcHgmm-n5qjemI",
+  authDomain: "forzzhub.firebaseapp.com",
+  projectId: "forzzhub",
+  storageBucket: "forzzhub.firebasestorage.app",
+  messagingSenderId: "339733324702",
+  appId: "1:339733324702:web:050def54e4a17ef7a02c8f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
