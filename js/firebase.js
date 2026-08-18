796388400f87ca0a0c0546f9934ec46109b20f30
forzita-x <arceleonel15@gmail.com>
2026-08-17 21:07:08 -0600

    last1

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,9 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAeaYdjjiIArA3dFOoyTjcHgmm-n5qjemI",
   authDomain: "forzzhub.firebaseapp.com",
@@ -8,5 +13,5 @@ const firebaseConfig = {
   appId: "1:339733324702:web:050def54e4a17ef7a02c8f"
 };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
