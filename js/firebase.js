0503bf81e1330bb74eeebe9ffd55622a6e1bd360
forzita-x <arceleonel15@gmail.com>
2026-08-18 19:56:34 -0600

    Full changes

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+// Configuración del proyecto ForzaStreetOD
 const firebaseConfig = {
   apiKey: "AIzaSyAeaYdjjiIArA3dFOoyTjcHgmm-n5qjemI",
   authDomain: "forzzhub.firebaseapp.com",
@@ -13,5 +8,8 @@ const firebaseConfig = {
   appId: "1:339733324702:web:050def54e4a17ef7a02c8f"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Inicializar Firebase usando el objeto global cargado desde el HTML
+firebase.initializeApp(firebaseConfig);
+
+// Crear la variable global "db" (Firestore) que necesita db.js
+var db = firebase.firestore();
